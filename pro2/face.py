30b327a635e51314d86404c29c31b0df216ac22f
jiyun1013 <rkf0645@naver.com>
2024-06-08 17:36:09 +0900

    서로 다른 사진으로 얼마나 일치하는지 비교하기 (-1 ~ 1)

--- pro2/face.py
+++ pro2/face.py
@@ -12,21 +12,23 @@ app.prepare(ctx_id=0, det_size=(640, 640))
 # STEP 3
 # img = ins_get_image('t1')
 img = cv2.imread('k1.jpeg', cv2.IMREAD_COLOR)
+img2 = cv2.imread('k2.jpg', cv2.IMREAD_COLOR)
 
 # STEP 4
-faces = app.get(img)
+faces1 = app.get(img)
+faces2 = app.get(img2)
 
 # STEP 5
 # then print all-to-all face similarity
 feats = []
-feats.append(faces[0].normed_embedding)
-feats.append(faces[0].normed_embedding)
+feat1= np.array(faces1[0].normed_embedding, dtype=np.float32)
+feat2= np.array(faces2[0].normed_embedding, dtype=np.float32)
 
 # for face in faces:
 # feats.append(face.normed_embedding)
-feats = np.array(feats, dtype=np.float32)
-sims = np.dot(feats[0], feats[1].T)
-print(sims)
+# feats = np.array(feats, dtype=np.float32)
+sims = np.dot(feat1, feat2.T)
+print(f"sims:{sims}")
 
 # rimg = app.draw_on(img, faces)
 # cv2.imwrite("./k1_output.jpeg", rimg)
