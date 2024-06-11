716dbec9debb7f5113e317b77c140a6e4a9f83e5
jiyun1013 <rkf0645@naver.com>
2024-06-11 16:05:08 +0900

    파일 업로드 및 파일 내 대화를 통한 감정 도출

--- Proj4/test_api.py
+++ Proj4/test_api.py
@@ -1,24 +1,32 @@
-# STEP 1. import modules
-from fastapi import FastAPI, Form
-from transformers import pipeline
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+from typing import List
 
-
-#STEP 2. create inference instance
-tokenizer = AutoTokenizer.from_pretrained("nlp04/korean_sentiment_analysis_kcelectra")
-model = AutoModelForSequenceClassification.from_pretrained("nlp04/korean_sentiment_analysis_kcelectra")
-
-classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
 app = FastAPI()
 
-#STEP 3. prepare input data
-@app.post("/text/")
-async def text(text: str = Form()):
+# Load sentiment analysis model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("nlp04/korean_sentiment_analysis_kcelectra")
+model = AutoModelForSequenceClassification.from_pretrained("nlp04/korean_sentiment_analysis_kcelectra")
+classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
 
-#STEP 4. infrence
-    result = classifier(text)
+@app.post("/upload/")
+async def upload_files(files: List[UploadFile] = File(...)):
+    results = []
+    for file in files:
+        # Ensure that the uploaded file is a text file
+        if file.filename.endswith(".txt"):
+            contents = await file.read()
+            text = contents.decode("utf-8")
 
-#STEP 5. visulize
-    print(text)
-    print(result[0])
-    return {"result" : result}
+            # Perform sentiment analysis
+            result = classifier(text)
+
+            # Print the input text and the analysis result
+            print(text)
+            print(result[0])
+
+            results.append({"filename": file.filename, "sentiment": result})
+        else:
+            raise HTTPException(status_code=400, detail="Only text files (.txt) are allowed.")
+
+    return {"results": results}
